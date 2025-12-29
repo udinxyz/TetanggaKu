c08ff4a73c098414015167cce88e691dd22510b3
udinxyz <email.muhammadirfazitny@gmail.com>
2025-12-30 01:25:59 +0700

    Update project

--- app/src/main/java/com/example/tetanggaku/presentation/screens/TermsPrivacyScreen.kt
+++ app/src/main/java/com/example/tetanggaku/presentation/screens/TermsPrivacyScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
